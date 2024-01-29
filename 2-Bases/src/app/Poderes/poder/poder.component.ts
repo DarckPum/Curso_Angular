41799eb39c5d4583a526c0b10ceb089e3a21aaf8
TavoSS <foctavio494@gmail.com>
2024-01-29 15:05:40 -0600

    Completo
    Se le agregara el componente Poderes

---
+++ 2-Bases/src/app/Poderes/poder/poder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poder',
+  templateUrl: './poder.component.html',
+  styleUrls: ['./poder.component.css']
+})
+export class PoderComponent {
+
+}
