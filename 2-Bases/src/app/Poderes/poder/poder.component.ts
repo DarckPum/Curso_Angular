aae37dcd5bc5b7bde6723c77aedcee76fb306edb
TavoSS <foctavio494@gmail.com>
2024-01-31 14:51:04 -0600

    Avance
    CCC

--- 2-Bases/src/app/Poderes/poder/poder.component.ts
+++ 2-Bases/src/app/Poderes/poder/poder.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-poder',
+  selector: 'app-heroe-poder',
   templateUrl: './poder.component.html',
   styleUrls: ['./poder.component.css']
 })
 export class PoderComponent {
+  public d: string = 'Descripcion';
+  public u: string = 'Movilidad y arma.';
+  public p: number = 20;
+  public v: string = 'Duende verde, Octropus y Venom';
+
+  //Propiedades
+  get descu():string {
+    return this.u.toUpperCase();
+  }
+
+  //Metodo
+  getPoderDescripcion():string {
+    return `${ this.u } - ${ this.p } - ${ this.v }`
+  }
+
+  chageU(): void {
+    this.u = 'Arma y fuerza';
+  }
+  changeP(): void {
+    this.p = 90;
+  }
+  changeV(): void {
+    this.v = 'El mismo';
+  }
+
+  resetFrom1(): void {
+    this.u = 'Movilidad y arma.';
+    this.p = 20;
+    this.v = 'Duende verde, Octropus y';
+
+  }
+
 
 }
