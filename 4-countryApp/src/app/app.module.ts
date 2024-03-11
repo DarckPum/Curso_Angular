0785ba84bd17a16cdf5512af76a8b0b4832416db
Tavo26pum <foctavio494@gmail.com>
2024-03-11 13:44:05 -0600

    Final
    Seccion 9

--- 4-countryApp/src/app/app.module.ts
+++ 4-countryApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     BrowserModule,
     SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
